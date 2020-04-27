ddec85f82863ff1ca421ddc0f631f2b1d0f272f8
karol <karol@karolbrennan.com>
2020-04-27 14:40:51 -0500

    Up the limit on how many cards you can generate at a time

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -79,18 +79,11 @@ class CardGenerator extends React.Component {
   }
 
   get numberOfCardsOptions() {
-    return [
-      {value: '1', label: '1'},
-      {value: '2', label: '2'},
-      {value: '3', label: '3'},
-      {value: '4', label: '4'},
-      {value: '5', label: '5'},
-      {value: '6', label: '6'},
-      {value: '7', label: '7'},
-      {value: '8', label: '8'},
-      {value: '9', label: '9'},
-      {value: '10', label: '10'},
-    ]
+    let options = [];
+    for(let i = 0; i <= 100; i++){
+      options.push({value: i.toString(), label: i.toString()});
+    }
+    return options;
   }
 
   get colorOptions(){
