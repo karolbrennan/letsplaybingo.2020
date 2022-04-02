d9ed3ae4fbe0e2fdc7d23f0e39ef1acdd8b52ce8
karol <karol@karolbrennan.com>
2022-04-02 09:07:46 -0500

    Fix term in release notes

--- src/components/pages/ReleaseNotes.js
+++ src/components/pages/ReleaseNotes.js
@@ -28,7 +28,7 @@ class ReleaseNotes extends React.Component {
               <li>Fixed an issue where the bingo board was writing out the numbers with letters for certain languages.</li>
             </ul>
             {/* ---------------------------------- */}
-            <h2><span className="date">5/4/2020</span> | Thank yous and hotfixes</h2>
+            <h2><span className="date">5/4/2020</span> | Thank yous and bug fixes</h2>
             <p>Thank you so much to everyone who has reached out to me and has donated to the operating costs of the site. I appreciate all of your emails and
               comments and I love hearing from you. I didn't set out on creating this app to make lots of money from it it was just a fun little project in honor of my grandma.
               I'm happy just to cover the costs of keeping it running!</p>
@@ -42,7 +42,7 @@ class ReleaseNotes extends React.Component {
               while I attended school and worked full time to better my life and provide for my child. I'm happy to say that I now have a successful career as a web 
               developer/development manager. I came up from nothing and I like to give back every chance I get.</p>
               
-            <h4 className="margin-bottom-lg">Hotfixes</h4>
+            <h4 className="margin-bottom-lg">Bug fixes</h4>
             <h5 className="margin-top-none margin-bottom-sm">Unable to pause number calling on some devices</h5>
             <p className="margin-top-none margin-bottom-lg medium-text">I have dug into the code and believe I have this fixed, however I have personally been unable to replicate it so I'm kind of fixing it blindly.</p>
             
