b15608ebb4eb07d39c8604577db0b7c1ebc57adb
karol <karol@karolbrennan.com>
2022-05-23 17:32:36 -0500

    PatternDisplay: Formatting

--- src/components/subcomponents/PatternDisplay.js
+++ src/components/subcomponents/PatternDisplay.js
@@ -4,10 +4,9 @@
  *  5.21.2022
  *  This class is used to display game patterns
  */
-import React from 'react';
+import React from "react";
 
 class PatternDisplay extends React.Component {
-
   /*
    *  Render Pattern Function
    *  This will display a bingo card with a pattern active
@@ -20,18 +19,30 @@ class PatternDisplay extends React.Component {
         <h5>{pattern.label}</h5>
         <div className="bingopattern container notranslate">
           {Object.keys(pattern.pattern).map((letter, index) => {
-            return(
-              <div key={letter + index} className="row vertical-row text-center">
-                <div className="col dark-bg white-text"><span>{letter}</span></div>
+            return (
+              <div
+                key={letter + index}
+                className="row vertical-row text-center">
+                <div className="col dark-bg white-text">
+                  <span>{letter}</span>
+                </div>
                 {Object.keys(pattern.pattern[letter]).map((number, index) => {
-                  return(
-                    <div key={letter + number} className={pattern.pattern[letter][number] ? 'selected col' : 'col'}>
-                      {letter === "N" && index === 2 ? <span className="free-space">Free Space</span> : <span>&nbsp;</span>}
+                  return (
+                    <div
+                      key={letter + number}
+                      className={
+                        pattern.pattern[letter][number] ? "selected col" : "col"
+                      }>
+                      {letter === "N" && index === 2 ? (
+                        <span className="free-space">Free Space</span>
+                      ) : (
+                        <span>&nbsp;</span>
+                      )}
                     </div>
-                  )
+                  );
                 })}
               </div>
-            )
+            );
           })}
         </div>
       </section>
