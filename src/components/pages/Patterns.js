d876c436a15d0ae42a94675087c869d78aeeb8cd
karol <karol@karolbrennan.com>
2022-05-22 21:12:57 -0500

    Patterns: document and format

--- src/components/pages/Patterns.js
+++ src/components/pages/Patterns.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import PatternDisplay from '../subcomponents/PatternDisplay';
-import { getPresetPatterns } from '../../utils.js';
-import '../../sass/bingopattern.scss';
+import React from "react";
+import PatternDisplay from "../subcomponents/PatternDisplay";
+import Utilities from "../helpers/Utilities";
+import "../../sass/bingopattern.scss";
 
+/**
+ * Renders the patterns page which allows users
+ * to view a list of patterns and print them out
+ */
 class Patterns extends React.Component {
-
   render() {
-    const patterns = getPresetPatterns().splice(1);
-    return(
+    const patterns = Utilities.getPresetPatterns().splice(1);
+    return (
       <section id="pattern-list">
         <div className="container row">
           <div className="col padding-xxlg">
-            <h1>Patterns 
-                <button className="primaryBtn" onClick={() => {window.print();return false;}}>Print</button>
+            <h1>
+              Patterns
+              <button
+                className="primaryBtn"
+                onClick={() => {
+                  window.print();
+                  return false;
+                }}>
+                Print
+              </button>
             </h1>
             <div className="container">
               <div className="row patterns">
-                {patterns.map(pattern => {
-                  return(
-                    <div key={pattern.label} className="pattern-block col">
+                {patterns.map((pattern) => {
+                  return (
+                    <div
+                      key={pattern.label}
+                      className="pattern-block col">
                       <PatternDisplay pattern={pattern}></PatternDisplay>
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 export default Patterns;
