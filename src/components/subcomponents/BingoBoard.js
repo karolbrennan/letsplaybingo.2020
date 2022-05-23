d17f24459696c438045ee391a7c6ca2e114c1c6f
karol <karol@karolbrennan.com>
2022-05-23 17:28:48 -0500

    BingoBoard: Document and format

--- src/components/subcomponents/BingoBoard.js
+++ src/components/subcomponents/BingoBoard.js
@@ -1,45 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class BingoBoard extends Component {
-  constructor(props){
+  /**
+   * Constructor for this class,
+   *
+   * @param   {Object}  props  params passed from the calling component
+   */
+  constructor(props) {
     super(props);
     this.state = {
       board: props.board,
       manualMode: props.manualMode,
-      manualCall: props.manualCall
-    }
+      manualCall: props.manualCall,
+    };
   }
 
-  static getDerivedStateFromProps(props, state){
-    if(props !== state){
+  /**
+   * Gets state from calling component
+   *
+   * @param   {Object}  props  [props description]
+   * @param   {Object}  state  [state description]
+   *
+   * @return  {Object}         [return description]
+   */
+  static getDerivedStateFromProps(props, state) {
+    if (props !== state) {
       state = props;
     }
     return state;
   }
-  
-  render(){
-    return(
-      <div id="board" className="flex">
+
+  /**
+   * Renders the component
+   *
+   * @return  {JSX}  [return description]
+   */
+  render() {
+    return (
+      <div
+        id="board"
+        className="flex">
         {Object.keys(this.state.board).map((letter, i) => {
-          return(
-            <div key={"board-row-" + letter} className="row no-wrap set-size text-center notranslate">
+          return (
+            <div
+              key={"board-row-" + letter}
+              className="row no-wrap set-size text-center notranslate">
               <div className="col board-letter white-bg red-text">{letter}</div>
-              {
-                this.state.board[letter].map((number) => {
-                  return(
-                    <div key={number.display}
-                        className={number.active ?  "col ball active" : 
-                        number.called ?  "col ball called" : "col ball"}>
-                          {this.state.manualMode ? <button onClick={() => this.state.manualCall(number)}>{number.number}</button> : number.number}
-                    </div>
-                  )
-                })
-              }
+              {this.state.board[letter].map((number) => {
+                return (
+                  <div
+                    key={number.display}
+                    className={
+                      number.active
+                        ? "col ball active"
+                        : number.called
+                        ? "col ball called"
+                        : "col ball"
+                    }>
+                    {this.state.manualMode ? (
+                      <button onClick={() => this.state.manualCall(number)}>
+                        {number.number}
+                      </button>
+                    ) : (
+                      number.number
+                    )}
+                  </div>
+                );
+              })}
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 export default BingoBoard;
