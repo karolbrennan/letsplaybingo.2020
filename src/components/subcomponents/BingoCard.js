71b9392500d2a6b17ca889adb2bf5a552e8d0706
karol <karol@karolbrennan.com>
2022-05-23 17:30:37 -0500

    BingoCard: Documentation and formatting

--- src/components/subcomponents/BingoCard.js
+++ src/components/subcomponents/BingoCard.js
@@ -1,35 +1,48 @@
-import React from 'react';
-
+import React from "react";
+/**
+ * Renders the BingoCard component
+ */
 class BingoCard extends React.Component {
-
-  get freeSpace(){
-    return(
+  /**
+   * Returns a JSX element representing a freespace
+   *
+   * @return  {JSX}  [return description]
+   */
+  get freeSpace() {
+    return (
       <div className="freespace">
-        <span>Free</span><span>Space</span>
+        <span>Free</span>
+        <span>Space</span>
       </div>
-    )
+    );
   }
-  
-  render(){
+
+  /**
+   * Renders the component
+   */
+  render() {
     let card = this.props.card;
-    return(
+    return (
       <div className="row">
         {Object.keys(card).map((letter) => (
-            <div className="col" key={"let" + letter}>
-              <div className="card-letter">{letter}</div>
-              <div className="card-numbers">
-                {Object.values(card[letter]).map((number, index) => (
-                  <div className="card-number" key={letter + number}>
-                      {letter === "N" && index === 2 ? this.freeSpace : number}
-                  </div>
-                ))}
-              </div>
+          <div
+            className="col"
+            key={"let" + letter}>
+            <div className="card-letter">{letter}</div>
+            <div className="card-numbers">
+              {Object.values(card[letter]).map((number, index) => (
+                <div
+                  className="card-number"
+                  key={letter + number}>
+                  {/* If we are looking at the N column, check if it's the middle space (free space) */}
+                  {letter === "N" && index === 2 ? this.freeSpace : number}
+                </div>
+              ))}
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
-    )
+    );
   }
-
 }
 export default BingoCard;
