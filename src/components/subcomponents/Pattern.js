dc33977b882f51597a42c32f77d48c0b8086586c
karol <karol@karolbrennan.com>
2022-05-23 17:32:18 -0500

    Pattern: Formatting

--- src/components/subcomponents/Pattern.js
+++ src/components/subcomponents/Pattern.js
@@ -4,10 +4,9 @@
  *  4.19.2020
  *  This class is used to display and control the game pattern.
  */
-import React from 'react';
+import React from "react";
 
 class Pattern extends React.Component {
-
   /*
    *  Render Pattern Function
    *  This will display a bingo card where the user can create their own pattern
@@ -17,21 +16,40 @@ class Pattern extends React.Component {
     const pattern = JSON.parse(JSON.stringify(this.props.pattern.pattern));
 
     return (
-      <div id="bingopattern" className="notranslate">
+      <div
+        id="bingopattern"
+        className="notranslate">
         {Object.keys(pattern).map((letter, index) => {
-          return(
-            <div key={letter + index} className="row vertical-row text-center">
-              <div className="col dark-bg white-text"><span>{letter}</span></div>
+          return (
+            <div
+              key={letter + index}
+              className="row vertical-row text-center">
+              <div className="col dark-bg white-text">
+                <span>{letter}</span>
+              </div>
               {Object.keys(pattern[letter]).map((number, index) => {
-                return(
-                  <div key={letter + number} className={pattern[letter][number] ? 'selected col' : 'col'}
-                      onClick={(e) => this.props.update(pattern, letter, index, pattern[letter][number])}>
-                      {letter === "N" && index === 2 ? <span className="free-space">Free Space</span> : <span>&nbsp;</span>}
+                return (
+                  <div
+                    key={letter + number}
+                    className={pattern[letter][number] ? "selected col" : "col"}
+                    onClick={(e) =>
+                      this.props.update(
+                        pattern,
+                        letter,
+                        index,
+                        pattern[letter][number]
+                      )
+                    }>
+                    {letter === "N" && index === 2 ? (
+                      <span className="free-space">Free Space</span>
+                    ) : (
+                      <span>&nbsp;</span>
+                    )}
                   </div>
-                )
+                );
               })}
             </div>
-          )
+          );
         })}
       </div>
     );
